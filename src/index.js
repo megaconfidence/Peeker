e2b7e6ffb5ba11ad50b2d8758f79214128868ea3
Confidence Okoghenun <confidenceboi@gmail.com>
2020-01-06 09:26:04 +0100

    feat: Added image precaching

--- src/index.js
+++ src/index.js
@@ -4,6 +4,37 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const imgSrc = [
+  '/image/icon/badge.svg',
+  '/image/icon/alarm.svg',
+  '/image/icon/plus.svg',
+  '/image/icon/palate.svg',
+  '/image/icon/picture.svg',
+  '/image/icon/archive.svg',
+  '/image/icon/trash.svg',
+  '/image/icon/options.svg',
+  '/image/icon/undo.svg',
+  '/image/icon/redo.svg',
+  '/image/icon/pin.svg',
+  '/image/icon/pin_fill.svg',
+  '/image/icon/close.svg',
+  '/image/icon/search.svg',
+  '/image/icon/add_contact.svg',
+  '/image/icon/refresh.svg',
+  '/image/icon/settings.svg',
+  '/image/icon/hamburger.svg',
+  '/image/icon/bulb.svg',
+  '/image/icon/bell.svg'
+];
+
+ReactDOM.render(
+  <>
+    {imgSrc.map((d, i) => (
+      <img src={d} key={i} alt='' />
+    ))}
+  </>,
+  document.getElementById('imgcache')
+);
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
