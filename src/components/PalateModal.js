a0536a1732658d87edc75a0883c3c15ec335c63d
Confidence Okoghenun <confidenceboi@gmail.com>
2020-03-08 04:00:27 +0100

    fix: Centered check mark on palate modal pick color

--- src/components/PalateModal.js
+++ src/components/PalateModal.js
@@ -1,5 +1,5 @@
-import React, { forwardRef } from "react";
-import "./PalateModal.css";
+import React, { forwardRef } from 'react';
+import './PalateModal.css';
 
 const PalateModal = forwardRef(
   ({ changeBackgroundColor, currentColor }, ref) => {
@@ -9,149 +9,151 @@ const PalateModal = forwardRef(
     return (
       <div
         ref={ref}
-        tabIndex="0"
-        className="palate"
+        tabIndex='0'
+        className='palate'
         onFocus={({ target }) => {
           target.childNodes.forEach(child => {
-            if (child.value === currentColor) {
+            if (child.value === '#fff' && !currentColor) {
+              child.checked = true;
+            } else if (child.value === currentColor) {
               child.checked = true;
             }
           });
         }}
         onBlur={() => {
-          ref.current.classList.remove("open");
+          ref.current.classList.remove('open');
         }}
       >
         <input
-          type="radio"
-          name="color"
-          value="#fff"
-          className="palate__colorpreview white"
-          style={{ background: "#fff" }}
+          type='radio'
+          name='color'
+          value='#fff'
+          className='palate__colorpreview white'
+          style={{ background: '#fff' }}
           onClick={handleColorPick}
           onBlur={() => {
-            ref.current.classList.remove("open");
+            ref.current.classList.remove('open');
           }}
         />
         <input
-          type="radio"
-          name="color"
-          value="#f28b82"
-          className="palate__colorpreview"
-          style={{ background: "#f28b82" }}
+          type='radio'
+          name='color'
+          value='#f28b82'
+          className='palate__colorpreview'
+          style={{ background: '#f28b82' }}
           onClick={handleColorPick}
           onBlur={() => {
-            ref.current.classList.remove("open");
+            ref.current.classList.remove('open');
           }}
         />
         <input
-          type="radio"
-          name="color"
-          value="#fbbc0b"
-          className="palate__colorpreview"
-          style={{ background: "#fbbc0b" }}
+          type='radio'
+          name='color'
+          value='#fbbc0b'
+          className='palate__colorpreview'
+          style={{ background: '#fbbc0b' }}
           onClick={handleColorPick}
           onBlur={() => {
-            ref.current.classList.remove("open");
+            ref.current.classList.remove('open');
           }}
         />
         <input
-          type="radio"
-          name="color"
-          value="#fff475"
-          className="palate__colorpreview"
-          style={{ background: "#fff475" }}
+          type='radio'
+          name='color'
+          value='#fff475'
+          className='palate__colorpreview'
+          style={{ background: '#fff475' }}
           onClick={handleColorPick}
           onBlur={() => {
-            ref.current.classList.remove("open");
+            ref.current.classList.remove('open');
           }}
         />
         <input
-          type="radio"
-          name="color"
-          value="#ccfe90"
-          className="palate__colorpreview"
-          style={{ background: "#ccfe90" }}
+          type='radio'
+          name='color'
+          value='#ccfe90'
+          className='palate__colorpreview'
+          style={{ background: '#ccfe90' }}
           onClick={handleColorPick}
           onBlur={() => {
-            ref.current.classList.remove("open");
+            ref.current.classList.remove('open');
           }}
         />
         <input
-          type="radio"
-          name="color"
-          value="#a7feeb"
-          className="palate__colorpreview"
-          style={{ background: "#a7feeb" }}
+          type='radio'
+          name='color'
+          value='#a7feeb'
+          className='palate__colorpreview'
+          style={{ background: '#a7feeb' }}
           onClick={handleColorPick}
           onBlur={() => {
-            ref.current.classList.remove("open");
+            ref.current.classList.remove('open');
           }}
         />
         <input
-          type="radio"
-          name="color"
-          value="#cbf0f8"
-          className="palate__colorpreview"
-          style={{ background: "#cbf0f8" }}
+          type='radio'
+          name='color'
+          value='#cbf0f8'
+          className='palate__colorpreview'
+          style={{ background: '#cbf0f8' }}
           onClick={handleColorPick}
           onBlur={() => {
-            ref.current.classList.remove("open");
+            ref.current.classList.remove('open');
           }}
         />
         <input
-          type="radio"
-          name="color"
-          value="#aecbfa"
-          className="palate__colorpreview"
-          style={{ background: "#aecbfa" }}
+          type='radio'
+          name='color'
+          value='#aecbfa'
+          className='palate__colorpreview'
+          style={{ background: '#aecbfa' }}
           onClick={handleColorPick}
           onBlur={() => {
-            ref.current.classList.remove("open");
+            ref.current.classList.remove('open');
           }}
         />
         <input
-          type="radio"
-          name="color"
-          value="#d7aefb"
-          className="palate__colorpreview"
-          style={{ background: "#d7aefb" }}
+          type='radio'
+          name='color'
+          value='#d7aefb'
+          className='palate__colorpreview'
+          style={{ background: '#d7aefb' }}
           onClick={handleColorPick}
           onBlur={() => {
-            ref.current.classList.remove("open");
+            ref.current.classList.remove('open');
           }}
         />
         <input
-          type="radio"
-          name="color"
-          value="#fdcfe8"
-          className="palate__colorpreview"
-          style={{ background: "#fdcfe8" }}
+          type='radio'
+          name='color'
+          value='#fdcfe8'
+          className='palate__colorpreview'
+          style={{ background: '#fdcfe8' }}
           onClick={handleColorPick}
           onBlur={() => {
-            ref.current.classList.remove("open");
+            ref.current.classList.remove('open');
           }}
         />
         <input
-          type="radio"
-          name="color"
-          value="#e6c9a8"
-          className="palate__colorpreview"
-          style={{ background: "#e6c9a8" }}
+          type='radio'
+          name='color'
+          value='#e6c9a8'
+          className='palate__colorpreview'
+          style={{ background: '#e6c9a8' }}
           onClick={handleColorPick}
           onBlur={() => {
-            ref.current.classList.remove("open");
+            ref.current.classList.remove('open');
           }}
         />
         <input
-          type="radio"
-          name="color"
-          value="#e8eaed"
-          className="palate__colorpreview"
-          style={{ background: "#e8eaed" }}
+          type='radio'
+          name='color'
+          value='#e8eaed'
+          className='palate__colorpreview'
+          style={{ background: '#e8eaed' }}
           onClick={handleColorPick}
           onBlur={() => {
-            ref.current.classList.remove("open");
+            ref.current.classList.remove('open');
           }}
         />
       </div>
