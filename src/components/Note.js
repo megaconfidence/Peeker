8cdf4de66490c9cd93ca081a736006f88175fdb8
Confidence Okoghenun <confidenceboi@gmail.com>
2020-03-08 03:19:39 +0100

    fix: Fixed border for notes with white background

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,15 +1,15 @@
-import "./Note.css";
-import "./Notes.css";
-import moment from "moment";
-import request from "../helpers";
-import DatePicker from "./DatePicker";
-import LabelModal from "./LabelModal";
-import PalateModal from "./PalateModal";
-import { useSnackbar } from "notistack";
-import isEqual from "../helpers/isEual";
-import colorLog from "../helpers/colorLog";
-import React, { useRef, useState, useEffect, useCallback } from "react";
-import ContentEditable from "react-contenteditable";
+import './Note.css';
+import './Notes.css';
+import moment from 'moment';
+import request from '../helpers';
+import DatePicker from './DatePicker';
+import LabelModal from './LabelModal';
+import PalateModal from './PalateModal';
+import { useSnackbar } from 'notistack';
+import isEqual from '../helpers/isEual';
+import colorLog from '../helpers/colorLog';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
+import ContentEditable from 'react-contenteditable';
 
 const Note = ({
   id,
@@ -32,15 +32,15 @@ const Note = ({
   const [contentText, setContentText] = useState({ value: content });
 
   const applySearch = useCallback(async () => {
-    const regex = new RegExp(searchText, "gi");
+    const regex = new RegExp(searchText, 'gi');
     const titleMark = title
-      .replace(/\n$/g, "\n\n")
-      .replace(regex, "<mark>$&</mark>");
+      .replace(/\n$/g, '\n\n')
+      .replace(regex, '<mark>$&</mark>');
     setTitleText({ value: titleMark });
 
     const contentMark = content
-      .replace(/\n$/g, "\n\n")
-      .replace(regex, "<mark>$&</mark>");
+      .replace(/\n$/g, '\n\n')
+      .replace(regex, '<mark>$&</mark>');
     setContentText({ value: contentMark });
   }, [content, searchText, title]);
 
@@ -67,22 +67,22 @@ const Note = ({
   const palateModalRef = useRef(null);
 
   const toggleNoteOpenClose = () => {
-    if (noteRef.current.classList.contains("note--opened")) {
+    if (noteRef.current.classList.contains('note--opened')) {
       uploadChanges();
     }
-    noteRef.current.classList.toggle("note--closed");
-    noteRef.current.classList.toggle("note--opened");
-    noteOverlayRef.current.classList.toggle("note__overlay--close");
+    noteRef.current.classList.toggle('note--closed');
+    noteRef.current.classList.toggle('note--opened');
+    noteOverlayRef.current.classList.toggle('note__overlay--close');
   };
 
   const noteOvrlayCheck = e => {
-    if (!noteOverlayRef.current.classList.contains("note__overlay--close")) {
+    if (!noteOverlayRef.current.classList.contains('note__overlay--close')) {
       e.stopPropagation();
     }
   };
 
   const uploadChanges = async () => {
-    if (status === "note" || status === "archive") {
+    if (status === 'note' || status === 'archive') {
       const originalData = {
         title,
         color,
@@ -93,14 +93,14 @@ const Note = ({
       let payload = {
         pinned: pinState.value,
         color: noteColor.value,
-        title: titleText.value.replace(/<\/?mark>/gi, ""),
-        content: contentText.value.replace(/<\/?mark>/gi, "")
+        title: titleText.value.replace(/<\/?mark>/gi, ''),
+        content: contentText.value.replace(/<\/?mark>/gi, '')
       };
 
       // Make update is data has changed
       if (!isEqual(originalData, payload) || isLabelUpdated) {
-        colorLog("Uploading changes", "success");
-        const noteId = noteRef.current.getAttribute("data-note-id");
+        colorLog('Uploading changes', 'success');
+        const noteId = noteRef.current.getAttribute('data-note-id');
 
         payload = {
           ...payload,
@@ -108,15 +108,15 @@ const Note = ({
         };
 
         updateLocal(noteId, payload);
-        await request("put", `api/note/${noteId}`, payload);
+        await request('put', `api/note/${noteId}`, payload);
       }
     }
   };
 
   const deleteNote = async () => {
-    const noteId = noteRef.current.getAttribute("data-note-id");
+    const noteId = noteRef.current.getAttribute('data-note-id');
     deleteLocal(noteId);
-    await request("delete", `api/note/${noteId}`);
+    await request('delete', `api/note/${noteId}`);
     fetchData();
   };
 
@@ -130,32 +130,32 @@ const Note = ({
       label: noteLabel.data,
       pinned: pinState.value,
       color: noteColor.value,
-      title: titleText.value.replace(/<\/?mark>/gi, ""),
-      content: contentText.value.replace(/<\/?mark>/gi, "")
+      title: titleText.value.replace(/<\/?mark>/gi, ''),
+      content: contentText.value.replace(/<\/?mark>/gi, '')
     };
 
     updateLocal(noteId, payload);
-    await request("put", `api/note/${noteId}`, payload);
+    await request('put', `api/note/${noteId}`, payload);
     fetchData();
   };
   const restoreNote = (noteId, initSatus) => {
-    updateNoteStatus(noteId, "note");
-    undo(noteId, initSatus, "Note restored");
+    updateNoteStatus(noteId, 'note');
+    undo(noteId, initSatus, 'Note restored');
   };
   const archiveNote = noteId => {
-    updateNoteStatus(noteId, "archive");
-    undo(noteId, "note", "Note archived");
+    updateNoteStatus(noteId, 'archive');
+    undo(noteId, 'note', 'Note archived');
   };
   const trashNote = (noteId, initSatus) => {
-    updateNoteStatus(noteId, "trash");
-    undo(noteId, initSatus, "Note trashed");
+    updateNoteStatus(noteId, 'trash');
+    undo(noteId, initSatus, 'Note trashed');
   };
 
   const undo = (noteId, status, msg) => {
     enqueueSnackbar(msg, {
       action: key => (
         <div
-          style={{ color: "#ffeb3b", marginRight: "8px" }}
+          style={{ color: '#ffeb3b', marginRight: '8px' }}
           onClick={() => {
             updateNoteStatus(noteId, status);
             closeSnackbar(key);
@@ -186,7 +186,7 @@ const Note = ({
 
   const handleDeleteLabelClick = ({ target }) => {
     const { data } = noteLabel;
-    const value = target.getAttribute("data-value");
+    const value = target.getAttribute('data-value');
 
     const i = data.findIndex(d => d === value);
     data.splice(i, 1);
@@ -198,18 +198,18 @@ const Note = ({
 
   const labelModalOpenCLose = () => {
     const labelOverlay = createLabelOverlay.current;
-    labelOverlay.classList.toggle("hide");
-    if (!labelOverlay.classList.contains("hide")) {
-      const labelSearchbox = labelOverlay.querySelector("input");
+    labelOverlay.classList.toggle('hide');
+    if (!labelOverlay.classList.contains('hide')) {
+      const labelSearchbox = labelOverlay.querySelector('input');
       labelSearchbox.focus();
     }
   };
 
   const handleReminderDate = value => {
     const newDate = moment(value).format();
-    const noteId = noteRef.current.getAttribute("data-note-id");
+    const noteId = noteRef.current.getAttribute('data-note-id');
     const subscription =
-      JSON.parse(localStorage.getItem("PEEKER_SUBSCRIPTION")) || "";
+      JSON.parse(localStorage.getItem('PEEKER_SUBSCRIPTION')) || '';
 
     setReminderDate(newDate);
 
@@ -220,22 +220,22 @@ const Note = ({
       label: noteLabel.data,
       pinned: pinState.value,
       clientNow: moment().format(),
-      title: titleText.value.replace(/<\/?mark>/gi, ""),
-      content: contentText.value.replace(/<\/?mark>/gi, "")
+      title: titleText.value.replace(/<\/?mark>/gi, ''),
+      content: contentText.value.replace(/<\/?mark>/gi, '')
     };
 
     updateLocal(noteId, payload);
-    request("put", `api/note/${noteId}`, payload);
+    request('put', `api/note/${noteId}`, payload);
   };
 
   const handleAlarmiconClick = () => {
-    dpwrapper.current.classList.toggle("hide");
+    dpwrapper.current.classList.toggle('hide');
     if (
-      !dpwrapper.current.classList.contains("hide") &&
-      !localStorage.getItem("PEEKER_NOTIFICATION_ISPERMITTED")
+      !dpwrapper.current.classList.contains('hide') &&
+      !localStorage.getItem('PEEKER_NOTIFICATION_ISPERMITTED')
     ) {
       const snackbarKey = enqueueSnackbar(
-        "Please allow notifications to use this feature",
+        'Please allow notifications to use this feature',
         {
           persist: true
         }
@@ -246,7 +246,7 @@ const Note = ({
 
   const handleDeleteReminder = e => {
     e.stopPropagation();
-    setReminderDate("");
+    setReminderDate('');
   };
 
   const changeBackgroundColor = color => {
@@ -254,12 +254,12 @@ const Note = ({
   };
 
   const handlePalateClick = () => {
-    if (!palateModalRef.current.classList.contains("open")) {
+    if (!palateModalRef.current.classList.contains('open')) {
       setTimeout(() => {
         palateModalRef.current.focus();
       }, 100);
     }
-    palateModalRef.current.classList.toggle("open");
+    palateModalRef.current.classList.toggle('open');
   };
 
   useEffect(() => {
@@ -271,7 +271,7 @@ const Note = ({
   return (
     <div
       className={`note__overlay note__overlay--close ${
-        status === "trash" ? "trash" : status === "archive" ? "archive" : ""
+        status === 'trash' ? 'trash' : status === 'archive' ? 'archive' : ''
       }`}
       onClick={toggleNoteOpenClose}
       ref={noteOverlayRef}
@@ -280,50 +280,51 @@ const Note = ({
         ref={noteRef}
         data-note-id={id}
         style={{
-          background: noteColor.value
+          background: noteColor.value,
+          border: noteColor.value === '#fff' ? '1px solid #e0e0e0' : ''
         }}
-        className="note note--closed"
+        className='note note--closed'
         onClick={noteOvrlayCheck}
       >
-        <div className="note__head">
+        <div className='note__head'>
           <ContentEditable
-            placeholder="Title"
-            spellCheck="false"
+            placeholder='Title'
+            spellCheck='false'
             html={titleText.value}
             innerRef={titleTextRef}
             onChange={handleTitleInput}
-            className="note__head__titletext textarea--mod"
+            className='note__head__titletext textarea--mod'
           />
           <div
             data-img
             onClick={pinNote}
-            className="note__head__pin"
-            data-imgname={`pin${pinState.value ? "_fill" : ""}`}
+            className='note__head__pin'
+            data-imgname={`pin${pinState.value ? '_fill' : ''}`}
           />
         </div>
-        <div className="note__body">
-          <div className="note__body__content">
+        <div className='note__body'>
+          <div className='note__body__content'>
             <ContentEditable
-              spellCheck="false"
-              placeholder="Note"
+              spellCheck='false'
+              placeholder='Note'
               html={contentText.value}
               innerRef={contentTextRef}
               onChange={handleContentInput}
-              className="note__body__content__textarea textarea--mod"
+              className='note__body__content__textarea textarea--mod'
             />
-            <div className="note__body__content__label">
+            <div className='note__body__content__label'>
               {reminderDate ? (
                 <div
-                  className="note__body__content__label__tag note__body__content__label__tag--reminder"
+                  className='note__body__content__label__tag note__body__content__label__tag--reminder'
                   onClick={handleAlarmiconClick}
                 >
-                  <div data-img data-imgname="alarm" />
-                  <span className="text">
-                    {moment(reminderDate).format("MMMM Do YYYY, h:mm a")}
+                  <div data-img data-imgname='alarm' />
+                  <span className='text'>
+                    {moment(reminderDate).format('MMMM Do YYYY, h:mm a')}
                   </span>
                   <div
                     data-img
-                    data-imgname="close"
+                    data-imgname='close'
                     onClick={handleDeleteReminder}
                   />
                 </div>
@@ -331,36 +332,36 @@ const Note = ({
                 undefined
               )}
               {noteLabel.data.map((d, i) => (
-                <div key={i} className="note__body__content__label__tag">
-                  <span className="text">{d}</span>
+                <div key={i} className='note__body__content__label__tag'>
+                  <span className='text'>{d}</span>
                   <div
                     data-img
                     data-value={d}
-                    data-imgname="close"
+                    data-imgname='close'
                     onClick={handleDeleteLabelClick}
                   />
                 </div>
               ))}
             </div>
-            <div className="note__body__content__edited">
-              {status === "trash" ? "Note in Trash •" : ""} Edited{" "}
+            <div className='note__body__content__edited'>
+              {status === 'trash' ? 'Note in Trash •' : ''} Edited{' '}
               {moment(updatedAt).fromNow()}
             </div>
           </div>
-          <div className="note__body__controls" tabIndex="0">
-            <div className="note__body__controls__item ">
+          <div className='note__body__controls' tabIndex='0'>
+            <div className='note__body__controls__item '>
               <div
                 data-img
-                data-imgname="alarm"
-                className="note__body__controls__item__image"
+                data-imgname='alarm'
+                className='note__body__controls__item__image'
                 onClick={handleAlarmiconClick}
               />
-              <div className="note__body__controls__item__withmodal">
+              <div className='note__body__controls__item__withmodal'>
                 <div
                   data-img
-                  data-imgname="badge"
+                  data-imgname='badge'
                   onClick={labelModalOpenCLose}
-                  className="note__body__controls__item__image"
+                  className='note__body__controls__item__image'
                 />
                 <LabelModal
                   allLabels={allLabels}
@@ -370,12 +371,12 @@ const Note = ({
                   updateNoteLabelAndStatus={updateNoteLabelAndStatus}
                 />
               </div>
-              <div className="note__body__controls__item__withmodal">
+              <div className='note__body__controls__item__withmodal'>
                 <div
                   data-img
-                  data-imgname="palate"
+                  data-imgname='palate'
                   onClick={handlePalateClick}
-                  className="note__body__controls__item__image"
+                  className='note__body__controls__item__image'
                 />
                 <PalateModal
                   ref={palateModalRef}
@@ -385,18 +386,18 @@ const Note = ({
               </div>
               <div
                 data-img
-                data-imgname="picture"
-                className="note__body__controls__item__image disabled"
+                data-imgname='picture'
+                className='note__body__controls__item__image disabled'
               />
-              {status !== "archive" ? (
+              {status !== 'archive' ? (
                 <div
                   data-img
                   data-note-id={id}
-                  data-imgname="archive"
+                  data-imgname='archive'
                   onClick={({ target }) => {
-                    archiveNote(target.getAttribute("data-note-id"));
+                    archiveNote(target.getAttribute('data-note-id'));
                   }}
-                  className="note__body__controls__item__image"
+                  className='note__body__controls__item__image'
                 />
               ) : (
                 undefined
@@ -404,21 +405,21 @@ const Note = ({
               <div
                 data-img
                 data-note-id={id}
-                data-imgname="trash"
+                data-imgname='trash'
                 onClick={({ target }) => {
-                  trashNote(target.getAttribute("data-note-id"), `${status}`);
+                  trashNote(target.getAttribute('data-note-id'), `${status}`);
                 }}
-                className="note__body__controls__item__image"
+                className='note__body__controls__item__image'
               />
               <div
                 data-img
-                data-imgname="undo"
-                className="note__body__controls__item__image disabled"
+                data-imgname='undo'
+                className='note__body__controls__item__image disabled'
               />
               <div
                 data-img
-                data-imgname="redo"
-                className="note__body__controls__item__image disabled"
+                data-imgname='redo'
+                className='note__body__controls__item__image disabled'
               />
             </div>
           </div>
@@ -429,9 +430,9 @@ const Note = ({
             allowNotifSBKey={allowNotifSBKey}
           />
         </div>
-        <div className="note__footer">
+        <div className='note__footer'>
           <button
-            className="note__footer__closebtn"
+            className='note__footer__closebtn'
             onClick={e => {
               e.stopPropagation();
               toggleNoteOpenClose();
@@ -440,12 +441,12 @@ const Note = ({
             Close
           </button>
 
-          {status === "trash" || status === "archive" ? (
+          {status === 'trash' || status === 'archive' ? (
             <button
               data-note-id={id}
-              className="note__footer__closebtn"
+              className='note__footer__closebtn'
               onClick={({ target }) => {
-                restoreNote(target.getAttribute("data-note-id"), `${status}`);
+                restoreNote(target.getAttribute('data-note-id'), `${status}`);
               }}
             >
               Restore
@@ -454,12 +455,12 @@ const Note = ({
             undefined
           )}
 
-          {status === "trash" ? (
+          {status === 'trash' ? (
             <button
               data-note-id={id}
               onClick={deleteNote}
-              className="note__footer__closebtn"
-              style={{ color: "rgba(255, 0, 0, 0.75)" }}
+              className='note__footer__closebtn'
+              style={{ color: 'rgba(255, 0, 0, 0.75)' }}
             >
               Delete
             </button>
